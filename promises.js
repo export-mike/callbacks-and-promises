8851b0653e03151a3f1882305cfd6d5e49eb6116
Michael James <mike.james@redant.com>
2015-08-05 13:04:59 +0100

    i guess that'll do

--- promises.js
+++ promises.js
@@ -1,51 +1,35 @@
 'use strict';
 var Person = require('./person');
 
-var person = new Person({
+Person.save({
 	username:'mjames',
 	password:'123', 
 	name:'mike',
 	age:24,
 	location:'london'
-}).save()
-.then(function(result){
+})
+.then(function(person){
 	console.log('SAVED PERSON');
-	console.log(result);
+	console.log(person);
 	console.log('\n');
+
+	person.password = '1234';
+	person.save()
+	.then(function(person){
+		console.log('Updated PERSON password');
+		console.log(person);
+		console.log('\n');
+
+		person.username = 'mikejames';
+		return person.save();
+	})
+	.then(function(person){
+		console.log('Updated PERSON username');
+		console.log(person);
+		console.log('\n');
+
+	});
 })
 .catch(function(err){
 	console.log(err);
 });
-
-// Person.get('mike',function(err, person){
-// 	if(err){
-// 		console.log(err);
-// 		console.log('error getting person');
-// 		return;
-// 	}
-
-// 	person.password = '1234';
-
-// 	person.save(function(err, person){
-// 		if(err){
-// 			console.log('error saving new password');
-// 			return;
-// 		}
-// 		console.log('Updated PERSON');
-// 		console.log(person);
-// 		console.log('\n');
-
-// 		person.username = 'mikejames';
-
-// 		person.save(function(err, person){
-// 			if(err){
-// 				console.log('error saving new password');
-// 				return;
-// 			}
-// 			console.log('Updated PERSON');
-// 			console.log(person);
-// 			console.log('\n');
-// 		});
-
-// 	});
-// });
